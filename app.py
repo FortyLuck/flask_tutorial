5824e414be2b2bc59099b231b610669268edc3b6
Dmitry Utkin <dm.utkin@gmail.com>
2021-12-06 22:43:20 +0300

    echo server

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 #! /usr/bin/python3
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, Dmitry!</p>"
+    
+@app.route("/bot", methods=["POST"])
+def bot():
+    if request.json:
+        print(request.json)
+        return request.json
+    
+    return "It's not json"
